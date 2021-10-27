33d9653024eda086a0165c08586a783cb20a0001
Kirill Shevchenko <kir971@gmail.com>
2021-10-27 20:12:20 +0300

    Add project files.

---
+++ 20211027_QueueList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211027_QueueList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueList queueList = new QueueList();
+
+            queueList.Put(10);
+            queueList.Put(20);
+            queueList.Put(30);
+
+            Console.WriteLine(queueList.GetData());
+            Console.WriteLine(queueList.GetData());
+            Console.WriteLine(queueList.GetData());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 20211027_QueueList/QueueList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211027_QueueList
+{
+    class QueueList
+    {
+        private Entry _firstData = null;
+        private Entry _endData = null;
+
+        public void Put(object data)
+        {
+            if(_firstData == null && _endData == null)
+            {
+                _firstData = new Entry(_firstData, data);
+                _endData = new Entry(_endData, data);
+            }
+            else
+            {
+                _endData = new Entry(_endData, data);
+                _firstData.Next = _endData.Next;            
+            }
+          
+        }
+
+        public object GetData()
+        {
+            if (_firstData == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            object result = _firstData.Data;
+            _endData = _endData.Next;
+
+            return result;
+        }
+
+        class Entry
+        {
+            private Entry _next;
+            private object _data;
+
+            public Entry(Entry next, object data)
+            {
+                _next = next;
+                _data = data;
+            }
+
+            public object Data
+            { 
+                get
+                {
+                    return _data;
+                }
+                set
+                {
+                    _data = value;
+                }
+            }
+
+            public Entry Next
+            {
+                get
+                {
+                    return _next;
+                }
+                set
+                {
+                    _next = value;
+                }
+            }
+
+        }
+    }
+}
